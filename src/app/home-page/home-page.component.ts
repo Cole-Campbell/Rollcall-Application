46138fdd09329d91a4ef5ac1e53ebeaeea6d44fd
cole-campbell <cole.william.campbell@gmail.com>
2017-03-15 10:55:12 +0000

    User can create a submission for Firebase using the angular HTTP service using the POST Method.

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
+
+import { Response } from '@angular/http';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styles: []
+  styles: [],
+  providers: [HttpService]
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent /*implements OnInit*/ {
 
-  constructor() { }
+  constructor(private httpService: HttpService) { }
 
-  ngOnInit() {
+  /*ngOnInit() {
+    //Calls Observable, but needs to be subscribed to.
+    //Callback is either data comes back, an error, or complete.
+    this.httpService.getData()
+      .subscribe(
+        (data: any) => console.log(data)
+      );
+  }*/
+
+  onSubmit(username: string, email: string) {
+    this.httpService.sendData({username: username, email:email})
+      .subscribe(
+        data=> console.log(data)
+      );
   }
-
 }
