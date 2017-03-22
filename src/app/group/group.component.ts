d15f95fbad8f2819a5022ca5782eacaf993930bb
cole-campbell <cole.william.campbell@gmail.com>
2017-03-22 11:16:44 +0000

    Displaying individual group content when a user navigates to a group.

--- src/app/group/group.component.ts
+++ src/app/group/group.component.ts
@@ -1,23 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFire, FirebaseObjectObservable } from 'angularfire2';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-group',
-  template: `
-    <h1>{{ (groups | async | json)?.groupId }}</h1>
-  `,
+  templateUrl: './group.component.html',
   styleUrls: ['./group.component.css']
 })
 export class GroupComponent implements OnInit {
 
   groupId: String;
 
-  groups: FirebaseObjectObservable<any>;
+  groups: FirebaseListObservable<any>;
 
   constructor(public af: AngularFire, private router: Router) {
-    this.groups = af.database.object('/Groups');
+    this.groups = af.database.list('/Groups');
   }
 
   ngOnInit() {
